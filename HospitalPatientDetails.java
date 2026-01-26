cb0472de3062476b539d7a8c877e480aa03f7860
rohithsoundarraj <rohithsoundarraj2@gmail.com>
2026-01-26 10:59:42 +0530

    Add files via upload

---
+++ HospitalPatientDetails.java
@@ -0,0 +1,20 @@
+public class HospitalPatientDetails {
+public static void main(String[] args) {
+byte patientType = 2;
+short roomNumber = 305;
+int patientId = 789456;
+long healthCardNumber = 123456789012L;
+float bodyTemperature = 98.6f;
+double treatmentCost = 45230.75;
+char bloodGroup = 'O';
+boolean isAdmitted = true;
+System.out.println("Patient Type: " + patientType);
+System.out.println("Room Number: " + roomNumber);
+System.out.println("Patient ID: " + patientId);
+System.out.println("Health Card Number: " + healthCardNumber);
+System.out.println("Body Temperature: " + bodyTemperature);
+System.out.println("Treatment Cost: ₹" + treatmentCost);
+System.out.println("Blood Group: " + bloodGroup);
+System.out.println("Is Admitted: " + isAdmitted);
+}
+}
